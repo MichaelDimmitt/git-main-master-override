f55a36149d5d6938919c22afce964eebca850bb7
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-04-10 16:02:48 -0400

    grab the error and check the branch it is complaining about

--- git_checkout_branch_override.sh
+++ git_checkout_branch_override.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-errormessage=$(git checkout main 2>&1)
-echo $errormessage
+main() {
+    errormessage=$(git checkout main 2>&1)
+    checkBranch $errormessage
+}
+## posix compliant solution:
+checkBranch() {
+    result=$*
+    if [ -z "${result##*master*}" ] ;then
+        git checkout main
+    elif [ -z "${result##*main*}" ] ;then
+        git checkout master
+    else
+        echo "$result"
+    fi
+}
+main

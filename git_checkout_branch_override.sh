86610f82756b09586da141226b5d661c09c475b0
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-04-10 16:41:57 -0400

    use exec to dynamically run command and capture error

--- git_checkout_branch_override.sh
+++ git_checkout_branch_override.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 branchOverride() {
     gitCommand="$*"
-    errormessage=$(gitCommand 2>&1)
-    checkBranch $errormessage
+    errormessage=$(echo "$gitCommand" | exec 2>&1)
+    checkBranch "$errormessage"
 }
 ## posix compliant solution:
 checkBranch() {
     result=$*
-    if [ -z "${result##*master*}" ] ;then
+    if [ -z "${result##*'master'*}" ] ;then
         command git checkout main
-    elif [ -z "${result##*main*}" ] ;then
+    elif [ -z "${result##*'main'*}" ] ;then
         command git checkout master
     else
         echo "$result"
@@ -17,7 +17,7 @@ checkBranch() {
 }
 git() {
   if [ "$1" == "checkout" ]; then
-    branchOverride "$*";
+    branchOverride "git $*";
   fi
 }
 git checkout main

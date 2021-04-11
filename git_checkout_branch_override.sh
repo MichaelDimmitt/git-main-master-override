e2e94214f05f3749cac2f84d4d7c25417925f8fb
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-04-10 20:03:21 -0400

    use environment logic instead of error logic.

--- git_checkout_branch_override.sh
+++ git_checkout_branch_override.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 branchOverride() {
-    gitCommand="$*"
-    errormessage=$(echo "$gitCommand" | exec 2>&1)
-    checkBranch "$errormessage"
-}
-## posix compliant solution:
-checkBranch() {
-    result=$*
-    if [ -z "${result##*'master'*}" ] ;then
-        command git checkout main
-    elif [ -z "${result##*'main'*}" ] ;then
-        command git checkout master
+    projectBranch=$(git branch | grep -E 'master|main' | head -n 1 | sed 's/*//');
+
+    # what branch did user request?
+    substr1="master"
+    substr2="main"
+    string="$*"
+
+    if [ -z "${string##*$substr1*}" ] ;then
+        command git checkout $projectBranch
+    elif [ -z "${string##*$substr2*}" ] ;then
+        command git checkout $projectBranch
     else
-        echo "$result"
+        command git $*
     fi
 }
 git() {
-  if [ "$1" == "checkout" ]; then
-    branchOverride "git $*";
+  if [ "$1" == "check" ]; then
+    bash <(curl -s https://raw.githubusercontent.com/MichaelDimmitt/git_check_computer/master/git_check_computer.sh)
+  elif [ "$1" == "add" ]; then
+    $(which git) "$@"
+    if [ -f ".hide_tracked" ]; then
+      while IFS='' read -r LINE || [ -n "${LINE}" ]; do
+        $(which git) reset HEAD $LINE;
+      done < .hide_tracked
+    fi
+  elif [ "$1" == "checkout" ]; then
+    branchOverride "$*";
+  else
+    $(which git) "$@"
   fi
 }
-git checkout master
